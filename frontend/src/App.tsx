3660400eb6c6f73c3fee853b06b4a1269e11ef84
Yohan Toledo <yohan.gabriel.bernart@gmail.com>
2021-11-02 03:17:21 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer'
+import DataTable from 'components/DataTable';
 
 function App() {
   return (
-    <div>
+   <>
+    <NavBar />
+    <div className="container"> 
       <h1 className="text-primary">Iniciando projeto!</h1>
+      <DataTable />
     </div>
+    
+
+    <Footer />
+  </>
   );
 }
 
